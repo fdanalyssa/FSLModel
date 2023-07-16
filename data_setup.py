949d8db2e47eed8d3c3b7b9d1631e70db48ce981
fdanalyssa <71048193+fdanalyssa@users.noreply.github.com>
2023-07-16 20:46:34 +0800

    Update data_setup.py

--- data_setup.py
+++ data_setup.py
@@ -4,31 +4,25 @@
 from torch.utils.data import DataLoader
 from torchvision import transforms as transforms
 from torchvision.datasets import ImageFolder
-from classnames import CLASS_NAMES
-
-#Auto-Transform
-#weights = models.MobileNet_V2_Weights.DEFAULT
-
-#auto_transforms = weights.transforms()
-
 
 #Augmenting the dataset
 def data_transform():
 
-    train_transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.RandomResizedCrop(size=256, scale=(0.8, 1.0)),
+    train_transform = transforms.Compose([  
+        transforms.Resize(224),
+        transforms.RandomResizedCrop(size=224, scale=(0.8, 1.0)),
         transforms.RandomRotation(degrees=15),
-        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(),
         transforms.CenterCrop(size=224),
         transforms.ToTensor(),
         transforms.Normalize(mean = [0.485, 0.456, 0.406],
                                 std = [0.229, 0.224, 0.225])
         ])
 
-    val_transform = transforms.Compose([
+
+    val_transform = transforms.Compose([ 
+        transforms.Resize(224), 
         transforms.CenterCrop(224),
-        transforms.Resize(256),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])       
         ])
@@ -37,7 +31,7 @@ def data_transform():
 
 
 #Uses ImageFolder to create the datasets
-def dataset_setup(train_transform, val_transform, train_dir = "FSL_alphabet/train", val_dir = "FSL_alphabet/val"):
+def dataset_setup(train_transform, val_transform, train_dir = "FSL_Dataset/train", val_dir = "FSL_Dataset/val"):
 
     #datasets
     train_dataset = ImageFolder(root=train_dir,
@@ -52,8 +46,6 @@ def dataset_setup(train_transform, val_transform, train_dir = "FSL_alphabet/trai
 #Turns the datasets into dataloaders
 def FSLdataloader(train_dataset, val_dataset, batch_size=32, num_workers=4, pin_memory=True):
 
-    class_names = CLASS_NAMES
-
     #dataloader
     train_dataloader = DataLoader(dataset=train_dataset, batch_size=batch_size, 
                                 shuffle=True, num_workers=num_workers, pin_memory=pin_memory)
@@ -61,7 +53,7 @@ def FSLdataloader(train_dataset, val_dataset, batch_size=32, num_workers=4, pin_
     val_dataloader = DataLoader(dataset=val_dataset, batch_size=batch_size, 
                                 shuffle=False, num_workers=num_workers, pin_memory=pin_memory)
 
-    return train_dataloader, val_dataloader, class_names
+    return train_dataloader, val_dataloader
 
  
 
