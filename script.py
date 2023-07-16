3b3e4215a80f09e3c62a8432339c4b22a77ab33c
fdanalyssa <71048193+fdanalyssa@users.noreply.github.com>
2023-07-16 20:45:05 +0800

    Update script.py

--- script.py
+++ script.py
@@ -2,8 +2,9 @@ import torch
 import torch.nn as nn
 import torchvision.models 
 from torch.utils.mobile_optimizer import optimize_for_mobile
-import utils as utils
 from classnames import CLASS_NAMES
+from utils import load_model as load_model
+from utils import check_model as check_model
 
 def script_model(model):
 
@@ -19,25 +20,26 @@ def script_model(model):
     traced_model = torch.jit.trace(model, input_data)
 
     # Save the TorchScript model to a file
-    traced_model.save('saved_models/android_converted/traced_FILOSign_Adam_0.000325_0.2_50e_torch13_androidtest_state.pt')
+    traced_model.save('model.pt')
 
     traced_script_module_optimized = optimize_for_mobile(traced_model)
 
-    traced_script_module_optimized._save_for_lite_interpreter("saved_models/android_converted/androidTest_FILOSign_Adam_0.000325_0.2_50e_torch13convtorch09_androidtest_state.ptl")
-
+    traced_script_module_optimized._save_for_lite_interpreter("model.ptl")
+    
 
 def main():
 
     # Load the pretrained MobileNetV2 model
-    PATH = "saved_models/FILOSign_Adam_0.000325_0.2_50e_torch13_androidtest_state.pth"
+    PATH = "model.pth"
 
-    model = torchvision.models.mobilenet_v2()
+
+    model = torchvision.models.mobilenet_v2() 
 
     model.classifier = nn.Sequential(
         nn.Dropout(p=0.2, inplace=True),
         nn.Linear(in_features=1280, out_features=len(CLASS_NAMES), bias=True)).to(device='cuda:0')
-
-    model = utils.load_model(model, PATH)
+    
+    model = load_model(model=model, model_save_path=PATH)
 
     script_model(model)
 
@@ -47,3 +49,4 @@ def main():
 if __name__ == "__main__":
 
     main()
+
