bf17d15b867cf074447867abf02dc693ecb1bf8a
fdanalyssa <71048193+fdanalyssa@users.noreply.github.com>
2023-07-16 20:44:43 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,4 @@
-"""
-Trains a PyTorch image classification model using device-agnostic code.
 
-"""
 import torch
 import torch.nn as nn
 import torch.optim
@@ -12,23 +9,26 @@ from torchvision.models import MobileNet_V2_Weights
 
 from classnames import CLASS_NAMES
 
-#from torchinfo import summary
+
 
 def main():
+    
     # Setup hyperparameters
-    NUM_EPOCHS = 50
+    NUM_EPOCHS = 100
     BATCH_SIZE = 64
-    LEARNING_RATE = 0.000325
+    LEARNING_RATE = 0.001
     NUM_WORKERS = 4
     ARCHITECTURE = "CNN_MobileNetV2"
-    DATASET = "FSL_Resized_"
-    OPTIMIZER_NAME = "RMS"
-    PATH_WHOLE_MODEL = "saved_models/whole_model/FILOSign_RMS_0.000325_0.2_50e_torch13_androidtest.pth"
-    PATH_STATE_MODEL = "saved_models/FILOSign_RMS_0.000325_0.2_50e_torch13_androidtest_state.pth"
+    DATASET = "FSL_Dataset_2130_224"
+    OPTIMIZER_NAME = "RMSprop"
+    PATH_STATE_MODEL = f"Trained_Models/dict_state/model_FILOSign_{OPTIMIZER_NAME}_{LEARNING_RATE}_{NUM_EPOCHS}e_2130_final.pth"
+    CONFUSION_MATRIX_NAME = f"Trained_Models/confusion_matrix/cfm_FILOSign_{OPTIMIZER_NAME}_{LEARNING_RATE}_{NUM_EPOCHS}e_2130_final.png"
+    CSV_NAME = f"Trained_Models/csv/csv_FILOSIGN_{OPTIMIZER_NAME}_{LEARNING_RATE}_{NUM_EPOCHS}e_2130_final.csv"
+    
 
     # Setup directories
-    train_dir = "FSL_alphabet/train"
-    val_dir = "FSL_alphabet/val"
+    train_dir = "FSL_Dataset_2000_224/train"
+    val_dir = "FSL_Dataset_2000_224/val"
 
     # Setup target device
     device = "cuda:0"
@@ -39,10 +39,8 @@ def main():
     # Create datasets
     train_dataset, val_dataset = data_setup.dataset_setup(train_transform, val_transform, train_dir, val_dir)
 
-
-
-    # Create DataLoaders with help from data_setup.py
-    train_dataloader, val_dataloader, class_names = data_setup.FSLdataloader(train_dataset=train_dataset,
+    # Create DataLoaders
+    train_dataloader, val_dataloader = data_setup.FSLdataloader(train_dataset=train_dataset,
                                                     val_dataset=val_dataset,
                                                     batch_size=BATCH_SIZE,
                                                     num_workers=NUM_WORKERS,
@@ -51,18 +49,9 @@ def main():
     #Auto-Transform
     weights = MobileNet_V2_Weights.IMAGENET1K_V1
 
- #   torch.backends.quantized.engine = 'fbgemm'
-    
-    #Quantized Weights
-  #  quantized_weights = quantization.MobileNet_V2_QuantizedWeights.DEFAULT
-
 
     #Activates pretrained weights of model
     model = mobilenet_v2(weights=weights).to(device='cuda:0')
-  #  model = mobilenet_v2(pretrained=True).to(device='cuda:0')
-    
-
-   # model = quantization.mobilenet_v2(weights=quantized_weights, quantize=True).to(device='cuda:0')
 
     #Freeze all layers
     for param in model.features.parameters():
@@ -75,23 +64,21 @@ def main():
 
     #Getting the length of classnames
     output_size = len(CLASS_NAMES)
-
+        
     #Recreate the classifier layer and see it to the target device
     model.classifier = nn.Sequential(
         nn.Dropout(p=0.2, inplace=True),
         nn.Linear(in_features=1280, out_features=output_size, bias=True)).to(device='cuda:0')
 
-
     # Define loss and optimizer
     loss_fn = nn.CrossEntropyLoss()
 
     optimizer = torch.optim.RMSprop(model.parameters(), lr=LEARNING_RATE, momentum=0.9, weight_decay=1e-5)
-  #  optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=1e-5)
 
     # learning rate scheduler
-    #scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=9, gamma=0.1)
 
-    # Start training with help from engine.py
+    # Start training with help from engine.pys
     engine.train(model=model,
                 train_dataloader=train_dataloader,
                 test_dataloader=val_dataloader,
@@ -100,15 +87,15 @@ def main():
                 epochs= NUM_EPOCHS,
                 device=device,
                 lr=LEARNING_RATE, 
+                scheduler=scheduler,
                 architecture=ARCHITECTURE,
                 dataset=DATASET,
+                batch_size=BATCH_SIZE,
                 opt_name=OPTIMIZER_NAME,
-                model_path= PATH_STATE_MODEL
-                )    
-
-    #saving entire model
-    print(f"[INFO] Saving whole model to: {PATH_WHOLE_MODEL}")
-    torch.save(model, PATH_WHOLE_MODEL)
+                model_path= PATH_STATE_MODEL,
+                cfm_name= CONFUSION_MATRIX_NAME,
+                csv_name= CSV_NAME
+              )    
 
 
 if __name__ == "__main__":
